a5f3469066473f323488ce3d9cec0ec89df6435d
abdullah snosii <abdallahsnosy413@gmail.com>
2026-08-17 05:39:38 +0300

    update

--- codeNodejs/main.js
+++ codeNodejs/main.js
@@ -527,3 +527,18 @@ app.delete("/suppliers/:supplierId", async (req, res, next) => {
 
 // GRANT DELETE ON `small retailstore`.sales TO 'store_manager'@'localhost';
 // FLUSH PRIVILEGES;
+
+
+
+
+
+// ans bonus 
+
+// SELECT 
+//     v.customer_id, 
+//     COUNT(v.visit_id) AS count_no_trans
+// FROM Visits v
+// LEFT JOIN Transactions t 
+//     ON v.visit_id = t.visit_id
+// WHERE t.transaction_id IS NULL
+// GROUP BY v.customer_id;
